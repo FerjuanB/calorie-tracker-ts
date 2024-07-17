be6b3e4fb6ddd27f78917be8cdbae67df9c9c942
Ferjuan <fbatres@gmail.com>
2024-07-17 07:49:38 -0300

    finalizada refactoring con contextAPI, codigo mas limpio

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useReducer, useEffect, useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import Form from "./components/Form"
-import { activityReducer, initialState } from './reducers/activity-reducer'
 import ActivityList from './components/ActivityList'
 import CalorieTracker from './components/CalorieTracker'
 import { useActivity } from './hooks/useActivity'
@@ -44,15 +43,13 @@ const {state, dispatch} = useActivity()
             <section className='bg-gray-800 py-10'>
                 <div className='max-w-4xl mx-auto'>
                     <CalorieTracker 
-                        activities={state.activities}
                     />
                 </div>
             </section>
 
             <section className="p-10 mx-auto max-w-4xl">
                 <ActivityList 
-                    activities={state.activities}
-                    dispatch={dispatch}
+                  
                 />
             </section>
         </>
--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, Dispatch, ReactNode, useMemo, useReducer } from "react"
 import { ActivityActions, activityReducer,ActivityState,initialState } from "../reducers/activity-reducer"
+import { categories } from "../data/categories"
+import { Activity } from "../types"
 
 type ActivityProviderProps = {
     children: ReactNode
@@ -12,6 +14,8 @@ type ActivityContextProps = {
     caloriesConsumed:number
     caloriesBurned:number
     netCalories:number
+    categoryName: (category: Activity["category"]) => string[]
+    isEmptyActivities: boolean
 }
 
 export const ActivityContext = createContext<ActivityContextProps>(null!) 
@@ -21,6 +25,13 @@ const [state,dispatch] = useReducer(activityReducer, initialState)
 const caloriesConsumed = useMemo(() => state.activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [state.activities])
 const caloriesBurned = useMemo(() => state.activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [state.activities])
 const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [state.activities])
+
+
+const categoryName = useMemo(() => 
+    (category: Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '' )
+, [state.activities])
+
+const isEmptyActivities = useMemo(() => state.activities.length === 0, [state.activities])
     return (
 
         <ActivityContext.Provider value={{  
@@ -28,7 +39,9 @@ const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [state.acti
             dispatch,
             caloriesConsumed,
             caloriesBurned,
-            netCalories
+            netCalories,
+            categoryName,
+            isEmptyActivities
         }}>
             {children}
         </ActivityContext.Provider>
